18447c592b32e3628f0cfbac4cec95620acba48e
Florian Amsallem <florian.amsallem@epita.fr>
2019-02-19 15:45:50 +0100

    assimp import: add

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,30 @@
-#include <iostream>
-
-#include <assimp/cimport.h>      // Plain-C interface
 #include <assimp/postprocess.h>  // Post processing flags
 #include <assimp/scene.h>        // Output data structure
+#include <assimp/Importer.hpp>   // C++ importer interface
+#include <iostream>
 
-static bool DoTheImportThing(const char *pFile) {
-    // Start the import on the given file with some example postprocessing
-    // Usually - if speed is not the most important aspect for you - you'll t
-    // probably to request more postprocessing than we do in this example.
-    const aiScene *scene = aiImportFile(
+bool DoTheImportThing(const std::string& pFile) {
+    // Create an instance of the Importer class
+    Assimp::Importer importer;
+    // And have it read the given file with some example postprocessing
+    // Usually - if speed is not the most important aspect for you - you'll
+    // propably to request more postprocessing than we do in this example.
+    const aiScene* scene = importer.ReadFile(
         pFile, aiProcess_CalcTangentSpace | aiProcess_Triangulate |
                    aiProcess_JoinIdenticalVertices | aiProcess_SortByPType);
 
     // If the import failed, report it
     if (!scene) {
-        std::cerr << "failed to import scene" << std::endl;
+        std::cerr << importer.GetErrorString() << std::endl;
         return false;
     }
-
-    // We're done. Release all resources associated with this import
-    aiReleaseImport(scene);
+    // Now we can access the file's contents.
+    // DoTheSceneProcessing(scene);
+    // We're done. Everything will be cleaned up by the importer destructor
     return true;
 }
 
-int main(int argc, char *argv) {
+int main(int argc, char* argv) {
+    DoTheImportThing("tests/cube.obj");
     std::cout << "Hello world" << std::endl;
 }
