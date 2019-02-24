5de877b91c3759ea65a184001f5ff64506df1cbe
Florian Amsallem <florian.amsallem@epita.fr>
2019-02-24 18:25:54 +0100

    transforms: flatten node transforms

--- src/main.cc
+++ src/main.cc
@@ -7,20 +7,48 @@
 
 #include "ppm.hh"
 
-const aiScene& import_scene(const std::string& pFile) {
+Assimp::Importer importer;
+const aiScene* scene;
+void import_scene(const std::string& pFile) {
     // Create an instance of the Importer class
-    Assimp::Importer importer;
     // And have it read the given file with some example postprocessing
     // Usually - if speed is not the most important aspect for you - you'll
     // propably to request more postprocessing than we do in this example.
-    const aiScene* scene = importer.ReadFile(
+    scene = importer.ReadFile(
         pFile, aiProcess_CalcTangentSpace | aiProcess_Triangulate |
                    aiProcess_JoinIdenticalVertices | aiProcess_SortByPType);
 
     if (!scene)
         throw std::runtime_error(importer.GetErrorString());
+}
 
-    return *scene;
+using Face = std::vector<aiVector3D>;
+
+static void flatten_node(std::vector<Face>& res, aiMatrix4x4 transform,
+                         const struct aiNode* node) {
+    transform *= node->mTransformation;
+    auto local_tr_mat = transform.Transpose();
+
+    for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+        auto mesh = scene->mMeshes[node->mMeshes[i]];
+        for (unsigned int j = 0; j < mesh->mNumFaces; j++) {
+            auto face_vec = Face(3);
+            for (unsigned int k = 0; k < mesh->mFaces[j].mNumIndices; k++) {
+                auto vertice = mesh->mVertices[mesh->mFaces[j].mIndices[k]];
+                std::cout << vertice.x << ' ' << vertice.y << ' ' << vertice.z
+                          << '\n';
+                vertice *= local_tr_mat;
+                face_vec.push_back(vertice);
+                std::cout << vertice.x << ' ' << vertice.y << ' ' << vertice.z
+                          << '\n';
+            }
+            res.push_back(std::move(face_vec));
+        }
+    }
+
+    for (unsigned int i = 0; i < node->mNumChildren; i++) {
+        flatten_node(res, transform, node->mChildren[i]);
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -33,7 +61,7 @@ int main(int argc, char* argv[]) {
     const std::string input_scene{argv[1]};
     const char* output_path = argv[2];
 
-    auto scene = import_scene(input_scene);
+    import_scene(input_scene);
 
     auto open_mode = (std::fstream::in | std::fstream::out |
                       std::fstream::binary | std::fstream::trunc);
@@ -43,8 +71,9 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    Image res{42, 42};
+    Image resulting_image{42, 42};
 
-
-    image_render_ppm(res, out);
+    std::vector<Face> res;
+    flatten_node(res, aiMatrix4x4{}, scene->mRootNode);
+    image_render_ppm(resulting_image, out);
 }
