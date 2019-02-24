319cf3fae0e98737870f3e362aa6312ce031739e
Florian Amsallem <florian.amsallem@epita.fr>
2019-02-24 17:04:40 +0100

    parser: fix return type

--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,14 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 #include <assimp/postprocess.h>  // Post processing flags
 #include <assimp/scene.h>        // Output data structure
 #include <assimp/Importer.hpp>   // C++ importer interface
 
-#include "render.hh"
 #include "ppm.hh"
+#include "render.hh"
 
-
-bool DoTheImportThing(const std::string& pFile) {
+const aiScene& import_scene(const std::string& pFile) {
     // Create an instance of the Importer class
     Assimp::Importer importer;
     // And have it read the given file with some example postprocessing
@@ -19,26 +18,26 @@ bool DoTheImportThing(const std::string& pFile) {
         pFile, aiProcess_CalcTangentSpace | aiProcess_Triangulate |
                    aiProcess_JoinIdenticalVertices | aiProcess_SortByPType);
 
-    if (!scene) {
-        std::cerr << importer.GetErrorString() << std::endl;
-        return false;
-    }
+    if (!scene)
+        throw std::runtime_error(importer.GetErrorString());
 
-    // do stuff
-    return true;
+    return *scene;
 }
 
-
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " OUTPUT_PPM" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " INPUT_SCENE OUTPUT_PPM"
+                  << std::endl;
         return 1;
     }
 
-    const char *output_path = argv[1];
-    DoTheImportThing("tests/cube.obj");
+    const std::string input_scene{argv[1]};
+    const char* output_path = argv[2];
 
-    auto open_mode = (std::fstream::in | std::fstream::out | std::fstream::binary | std::fstream::trunc);
+    auto scene = import_scene(input_scene);
+
+    auto open_mode = (std::fstream::in | std::fstream::out |
+                      std::fstream::binary | std::fstream::trunc);
     auto out = std::fstream(output_path, open_mode);
     if (!out.is_open()) {
         std::cerr << "failed to open: " << output_path << std::endl;
